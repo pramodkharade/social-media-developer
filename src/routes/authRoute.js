df63653fe8ae129b6cc9760e7d56fad4da1c61a8
pramodkharade <kharade.pramod91@gmail.com>
2020-05-29 10:55:35 +0530

    auth route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
 // @route   GET api/auth/
-// @desc    auth user route
+// @desc    get user
 // @access  private
 router.get('/', auth, authController.getAuthenticate);
 
